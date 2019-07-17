1938c3bc60b75a62f323a675f5857ca584fa34ba
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-07-17 11:29:10 -0400

    hitting the API and returning result need to now debug and scale up

--- gifApp.js
+++ gifApp.js
@@ -6,30 +6,36 @@ console.log('app is running');
 
 $(() => { 
     const handleData = (data) =>    {
-        console.log(data)
+        console.log(data.data[0].url)//logging the data returned with the gif as the main visual
+
+    const $gif = $('<img>').attr(data.data[0].url)//is .text() the correct method?
+    $('.parent').append($gif)//append $gif to the parent div
+        console.log($gif)
+    //space for further details like title of gif etc. if need be
+
     }
 
-    const gifQuery = 'cars'
-    const endpoint = `http://api.giphy.com/v1/gifs/search?q=${gifQuery}&api_key=PSWZ8A3XhnWfE1CgCZMyJef5BM0xvI4L&limit=1` //query limited to one for now -- theoretical limit of 30 set by Giphy themselves
-  
-    console.log(endpoint)
+    // console.log(data)
+    // console.log(data.responseJSON) //undifined because js is asynchronous -- we need to use a promise "IOU"
     
-    const data = $.ajax({
-        url: endpoint
+    $('.searchButton').on('click', (event) =>  {//making on click event to tie button and search bar into returned results
+        event.preventDefault()//stop the page from reloading when not nessesary
+        console.log('submited')
+        const gifQuery = $('searchBar').val() //capturing value form input
+        const endpoint = `http://api.giphy.com/v1/gifs/search?q=${gifQuery}&api_key=PSWZ8A3XhnWfE1CgCZMyJef5BM0xvI4L&limit=1` //query limited to one for now -- theoretical limit of 30 set by Giphy themselves
+        
+        $.ajax({url: endpoint}).then(handleData) //making the ajax call get data asynchronously, when the data gets back, handle it
+        
     })
-    console.log(data)
-    console.log(data.responseJSON) //undifined because js is asynchronous -- we need to use a promise "IOU"
-    
-    $.ajax({  //making the ajax call
-        url: endpoint
-    }).then(handleData) // get data asynchronously, when the data gets back, handle it
-    
+}) 
+
     //working well thus far, going to push to git hub for working copy update
     //gifQuery plays well with the api, i have it limited to one for now for testing purposes
     //need to now capture the data, tie it to the seatch bar and buttons
     //append it to the dom and display it on the page
 
-})  
+    //make ajax requests dynamic 
+
 
 
 
