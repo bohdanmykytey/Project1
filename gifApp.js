b8d7b0a99af86271a93fbffa42e0f74bb1fe46c4
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-07-18 14:15:18 -0400

    div containing content centered, background updated, clear button added

--- gifApp.js
+++ gifApp.js
@@ -5,13 +5,16 @@ $(() => {
             $('ul').append($gif)//append $gif to the parent ul
         }
     }
-
     $('.searchButton').on('click', (event) =>  {//making on click event to tie button and search bar into returned results
         event.preventDefault()//stop the page from reloading when not nessesary
         const gifQuery = $('.searchBar').val() //capturing value form input
         console.log(gifQuery)
-        const endpoint = `http://api.giphy.com/v1/gifs/search?q=${gifQuery}&api_key=PSWZ8A3XhnWfE1CgCZMyJef5BM0xvI4L&limit=1000` //query limited to one for now -- theoretical limit of 30 set by Giphy themselves
+        const endpoint = `http://api.giphy.com/v1/gifs/search?q=${gifQuery}&api_key=PSWZ8A3XhnWfE1CgCZMyJef5BM0xvI4L&limit=100` //query limited to one for now -- theoretical limit of 30 set by Giphy themselves
         $.ajax({url: endpoint}).then(handleData) //making the ajax call get data asynchronously, when the data gets back, handle it
-        
     })
+    $('.clearButton').on('click', () => {
+        $('.parent').empty()//clear button works BUT it makes the code no longer responcive once its clicked
+
+    })
+    
 }) 
