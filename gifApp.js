b1e8f09d0d2352c6f06dfdec8ff13d93ed606c39
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-07-18 12:14:25 -0400

    all of the logic is complete

--- gifApp.js
+++ gifApp.js
@@ -1,29 +1,24 @@
-console.log('app is running');
-
-
-//after i make the call to the api and you get a gif back, make a div and put it on the page
-//start with one and move on further down.
-
 $(() => { 
     const handleData = (data) =>    {
-        console.log(data.data[0].url)//logging the data returned with the gif as the main visual
-
-    const $gif = $('<img>').attr(data.data[0].url)//is .text() the correct method?
-    $('.parent').append($gif)//append $gif to the parent div
-        console.log($gif)
-    //space for further details like title of gif etc. if need be
-
+        console.log(data)//logging the data returned with the gif as the main visual
+    
+        // const gifArray = data.data[0].images.original.url
+        
+        for(let i = 0; i < data.data.length; i++)  {
+            const $gif = $('<img>').attr('src', data.data[i].images.original.url)
+            $('ul').append($gif)//append $gif to the parent ul
+        }
+        // $('.parent').append(ul)
+    //space for further details like title of gif etc. if need be 
     }
-
     // console.log(data)
     // console.log(data.responseJSON) //undifined because js is asynchronous -- we need to use a promise "IOU"
-    
+
     $('.searchButton').on('click', (event) =>  {//making on click event to tie button and search bar into returned results
         event.preventDefault()//stop the page from reloading when not nessesary
-        console.log('submited')
-        const gifQuery = $('searchBar').val() //capturing value form input
-        const endpoint = `http://api.giphy.com/v1/gifs/search?q=${gifQuery}&api_key=PSWZ8A3XhnWfE1CgCZMyJef5BM0xvI4L&limit=1` //query limited to one for now -- theoretical limit of 30 set by Giphy themselves
-        
+        const gifQuery = $('.searchBar').val() //capturing value form input
+        console.log(gifQuery)
+        const endpoint = `http://api.giphy.com/v1/gifs/search?q=${gifQuery}&api_key=PSWZ8A3XhnWfE1CgCZMyJef5BM0xvI4L&limit=100` //query limited to one for now -- theoretical limit of 30 set by Giphy themselves
         $.ajax({url: endpoint}).then(handleData) //making the ajax call get data asynchronously, when the data gets back, handle it
         
     })
