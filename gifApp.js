86db4cb9f5649aecd5ddb6bc971d70c18a47a099
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-07-19 16:31:59 -0400

    project is done

--- gifApp.js
+++ gifApp.js
@@ -2,6 +2,7 @@ $(() => {
     const handleData = (data) =>    {
         for(let i = 0; i < data.data.length; i++)  {
             const $gif = $('<img>').attr('src', data.data[i].images.original.url)
+            $gif.addClass('baseGif')
             $('.parent').append($gif)//append $gif to the parent ul
             
         }
@@ -9,7 +10,6 @@ $(() => {
     $('form').on('submit', (event) =>  {//making on click event to tie button and search bar into returned results
         event.preventDefault()//stop the page from reloading when not nessesary
         const gifQuery = $('.searchBar').val() //capturing value form input
-        console.log(gifQuery)
         const endpoint = `https://api.giphy.com/v1/gifs/search?q=${gifQuery}&api_key=PSWZ8A3XhnWfE1CgCZMyJef5BM0xvI4L&limit=100` //query limited to one for now -- theoretical limit of 30 set by Giphy themselves
         $.ajax({url: endpoint}).then(handleData) //making the ajax call get data asynchronously, when the data gets back, handle it
     })
@@ -20,23 +20,20 @@ $(() => {
 
     
     $('.parent').on('click', 'img', (event) =>  {
-        const $imageTag = $('<img>')
+        const $imageTag = $('<img>').addClass("modalGif")
         $imageTag.attr('src', $(event.target).attr('src'))
         $('#modalContent').append($imageTag)
         $('#modal').css('display', 'block')
+        $('.parent').hide()
+        $('body').css('height', '5000px')
+        
         
         console.log('image clicked', event.target.id)
     })
 
-
-
-
-
-
-
-    // $('window').on('click', () =>   {
-    //     $('img').addClass('.close');
-    //     console.log('window clicked') 
-    // })
-    
+    $('#modalContent').on('click', (event) =>  {
+        $("#modal").css('display', 'none')
+        $('#modalContent').empty()
+        $('.parent').show()
+    })
 }) 
